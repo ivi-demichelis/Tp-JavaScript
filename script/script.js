8c55112fc824c186ac873f9f62c3a580e9810843
ivi-demichelis <ividem8686@gmail.com>
2022-10-26 19:22:18 -0300

    ultimos cambios

--- script/script.js
+++ script/script.js
@@ -2,77 +2,105 @@
 const copyIcon = document.getElementById("copy");
 const refreshIcon = document.getElementById("refresh");
 const passwordPlace = document.getElementById("passwordResult");
+const generateBtn = document.getElementById("button");
 
 const includeNumbers = document.getElementById("includeNumbers");
 const includeLower = document.getElementById("includeLower");
 const includeUpper = document.getElementById("includeUpper");
 const includeSymbols = document.getElementById("includeSymbols");
 
-
-
 const lettersOnly = document.querySelector("#lettersOnly");
 const numbersOnly = document.querySelector("#numbersOnly");
 const allCharacters = document.getElementById("allCharacters");
 
-
-    let base = "";
+    
     const lower = "abcdefghijklmnopqrstuvwxyz";
     const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numbers = "0123456789";
     const symbols = ".?,;-_¡!¿*%&$/()[]{}|@><";
-    
+
     const allowedCharacter = [lower, numbers, upper, symbols]
+    
 
 const generatePassword = (base, length) => {
-    let password = "";
+    let password = [];
     for (let i = 0; i < length; i++) {
         let random = Math.floor(Math.random() * base.length);
-        password += base.charAt(random);
+        password.push(base.charAt(random));    
     }
-    return password;
+    let finalPassword = password.join("")
+    return finalPassword
 };
 
 
 const generate = () => {
     let length = parseInt(numberRange.value);
 
+    let base = "";
+
     if (includeNumbers.checked) base += allowedCharacter[1];
     if (includeUpper.checked) base += allowedCharacter[2];
     if (includeLower.checked) base += allowedCharacter[0];
     if (includeSymbols.checked) base += allowedCharacter[3];
 
-    if (lettersOnly.checked) base = allowedCharacter[0] + allowedCharacter[2];
-    if (numbersOnly.checked) base = allowedCharacter[1];
-    if (allowedCharacter.checked) {
-       for (let index = 0; index < allowedCharacter.length; index++) {
-        base += allowedCharacter[index];
-        
-       }
-    }
     passwordPlace.innerText = generatePassword(base, length);
-
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-    boton.addEventListener("click", () => {
+    button.addEventListener("click", () => {
         generate();
     });
-    lettersOnly.addEventListener("input", () =>{
+    lettersOnly.addEventListener("click", () =>{
         includeLower.disabled = false;
         includeUpper.disabled = false;
+        includeLower.checked = true;
+        includeUpper.checked = true;
         includeNumbers.disabled = true;
         includeNumbers.checked = false;
         includeSymbols.disabled = true;
-        includeSymbols.checked = false;   
-   
+        includeSymbols.checked = false;      
     })
     
-    numbersOnly.addEventListener("input", () =>{})
-    allCharacters.addEventListener("input", () =>{})
+    numbersOnly.addEventListener("click", () =>{
+        includeLower.disabled = true;
+        includeLower.checked = false;
+        includeUpper.disabled = true;
+        includeUpper.checked = false;
+        includeSymbols.disabled = true;
+        includeSymbols.checked = false;
+        includeNumbers.disabled = false;
+        includeNumbers.checked = true;
+    })
+
+    allCharacters.addEventListener("click", () =>{
+        includeLower.disabled = false;
+        includeLower.checked = true;
+        includeUpper.disabled = false;
+        includeUpper.checked = true;
+        includeSymbols.disabled = false;
+        includeSymbols.checked = true;
+        includeNumbers.disabled = false;
+        includeNumbers.checked = true;
+    })
 });
 
+window.addEventListener("load", () => {
+    allCharacters.click()
+}) 
+
+
 refreshIcon.addEventListener("click", () => {
-   passwordPlace.value = generatePassword("");
+   passwordPlace.innerText = "";
+    includeLower.checked = false;
+    includeUpper.checked = false;
+    includeSymbols.checked = false;
+    includeNumbers.checked = false;
+    lettersOnly.checked = false;
+    numbersOnly.checked = false;
+    allCharacters.checked = false;
+    includeNumbers.disabled = false;
+    includeSymbols.disabled = false;
+    allCharacters.click()
  })
 
  copyIcon.addEventListener("click", () => {    
@@ -84,7 +112,11 @@ refreshIcon.addEventListener("click", () => {
     }
 })
 
-
+generateBtn.addEventListener("click", () =>{
+    if(allCharacters.checked = false){
+        alert("Debes elegir caracteres antes de generar una contraseña")
+    }
+})
 
 
 
